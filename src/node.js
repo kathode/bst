5dd3a247da84385b2f0f98ede810506aa0e34913
Kathode <work.qiyan@gmail.com>
2025-04-11 14:03:23 +1000

    Remove left and right parameters

--- src/node.js
+++ src/node.js
@@ -1,7 +1,7 @@
 export class Node {
-  constructor(data, left, right) {
+  constructor(data) {
     this.data = data;
-    this.left = left;
-    this.right = right;
+    this.left = null;
+    this.right = null;
   }
 }
